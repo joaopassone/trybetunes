f46730511ceab0781300c127c35edb018576845b
joaopassone <joao.passone@gmail.com>
2022-11-03 19:48:31 -0300

    Entrega: Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
 class Header extends Component {
   state = {
     name: null,
-  }
+  };
 
-  componentDidMount = async () => {
+  async componentDidMount() {
     const { name } = await getUser();
     this.setState(() => ({
       name,
     }));
-  };
+  }
 
   render() {
     const { name } = this.state;
@@ -20,6 +21,9 @@ class Header extends Component {
     return (
       <header data-testid="header-component">
         { name ? <p data-testid="header-user-name">{ name }</p> : <Loading /> }
+        <Link to="/search" data-testid="link-to-search" />
+        <Link to="/favorites" data-testid="link-to-favorites" />
+        <Link to="/profile" data-testid="link-to-profile" />
       </header>
     );
   }

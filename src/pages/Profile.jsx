e396852dabc530f81b9525bbd94272aeb6976750
joaopassone <joao.passone@gmail.com>
2022-11-05 14:14:06 -0300

    Entrega: Requisitos 13 e 14

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    onLoading: false,
+    user: {},
+  };
+
+  componentDidMount() {
+    this.setState({ onLoading: true }, async () => {
+      const user = await getUser();
+      this.setState({ onLoading: false, user });
+    });
+  }
+
   render() {
+    const { user, onLoading } = this.state;
+    const { name, email, image, description } = user;
+
+    const page = (
+      <div>
+        <img src={ image } alt="user" data-testid="profile-image" />
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
+      </div>
+    );
+
     return (
       <div data-testid="page-profile">
         <Header />
+        { onLoading ? <Loading /> : page }
       </div>
     );
   }
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,123 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
+  state = {
+    onLoading: false,
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+    isSaveButtonDisabled: true,
+    isSaved: false,
+  };
+
+  componentDidMount() {
+    this.setState({ onLoading: true }, async () => {
+      const user = await getUser();
+      const { name, email, image, description } = user;
+      this.setState({ onLoading: false, name, email, image, description }, () => {
+        this.validateSaveButton();
+      });
+    });
+  }
+
+  validateSaveButton = () => {
+    const { name, email, image, description } = this.state;
+
+    if (name.length * email.length * image.length * description.length !== 0
+      && email.match(/^.+@.+$/)) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.validateSaveButton();
+    });
+  };
+
+  onSaveButtonClick = () => {
+    const { name, email, image, description } = this.state;
+    this.setState({ onLoading: true }, async () => {
+      await updateUser({ name, email, image, description });
+      this.setState({ onLoading: false, isSaved: true });
+    });
+  };
+
   render() {
+    const { onLoading, name, email, image,
+      description, isSaveButtonDisabled, isSaved } = this.state;
+
+    const page = (
+      <form>
+        <label htmlFor="edit-input-name">
+          Nome
+          <input
+            type="text"
+            name="name"
+            id="edit-input-name"
+            value={ name }
+            onChange={ this.handleChange }
+            data-testid="edit-input-name"
+          />
+        </label>
+        <label htmlFor="edit-input-email">
+          Email
+          <input
+            type="text"
+            name="email"
+            id="edit-input-email"
+            value={ email }
+            onChange={ this.handleChange }
+            data-testid="edit-input-email"
+          />
+        </label>
+        <label htmlFor="edit-input-description">
+          Descrição
+          <textarea
+            name="description"
+            id="edit-input-description"
+            value={ description }
+            onChange={ this.handleChange }
+            data-testid="edit-input-description"
+          />
+        </label>
+        <label htmlFor="edit-input-image">
+          Imagem
+          <input
+            type="text"
+            name="image"
+            id="edit-input-image"
+            value={ image }
+            onChange={ this.handleChange }
+            data-testid="edit-input-image"
+          />
+        </label>
+        <button
+          type="button"
+          name="editButtonSave"
+          id="edit-button-save"
+          disabled={ isSaveButtonDisabled }
+          onClick={ this.onSaveButtonClick }
+          data-testid="edit-button-save"
+        >
+          Salvar
+        </button>
+      </form>
+    );
+
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        { isSaved ? <Redirect to="/profile" /> : null }
+        { onLoading ? <Loading /> : page }
       </div>
     );
   }
